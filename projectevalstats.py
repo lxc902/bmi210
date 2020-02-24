e7acf86caeb60b6fcfdbbcc10813d0038080a725
Sophia Y Wang <50113622+eyelovedata@users.noreply.github.com>
2020-02-24 10:27:24 -0800

    added F1 scores, fixed bug
    I realized my git client isn't pushing files correctly...sorry this is late

--- projectevalstats.py
+++ projectevalstats.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 #this just creates some fake data to test on 
-#df = pd.DataFrame(np.random.randint(0,3,size=(100, 2)), columns=["label", "predicted"])
+df = pd.DataFrame(np.random.randint(0,3,size=(100, 2)), columns=["label", "predicted"])
 
 
 def evalstats(df): 
@@ -18,16 +18,25 @@ def evalstats(df):
     print(pd.crosstab(df["predicted"], df["label"]))
     cm=np.array(pd.crosstab(df["predicted"], df["label"]))
     print("Accuracy:", round((cm[0,0]+cm[1,1]+cm[2,2])/cm.sum(),3))
+    
     print("**********Stats for 0 label vs 1/2*********")
-    print("Sensitivity (aka Recall): ",round(cm[0,0]/cm[:,0].sum(),3))
+    Recall=cm[0,0]/cm[:,0].sum()
+    print("Sensitivity (aka Recall): ",round(Recall,3))
     print("Specificity: ",round(cm[1:3,1:3].sum()/cm[0:3,1:3].sum(),3))
-    print("PPV (aka Precision): ",round(cm[0,0]/cm[0].sum(),3))
+    Precision=cm[0,0]/cm[0].sum()
+    print("PPV (aka Precision): ",round(Precision,3))
     print("NPV: ",round(cm[1:3,1:3].sum()/cm[1:3,0:3].sum(),3))
+    print("F1 Score: ",round(2*Precision*Recall/(Precision+Recall),3))
+
 
     print("**********Stats for 0/1 label vs 2*********")
-    print("Sensitivity (aka Recall): ",round(cm[0,0].sum()/cm[:,0].sum(),3))
+    Recall=cm[0:2,0:2].sum()/cm[:,0:2].sum()
+    print("Sensitivity (aka Recall): ",round(Recall,3))
     print("Specificity: ",round(cm[2,2]/cm[:,2].sum(),3))
-    print("PPV (aka Precision): ",round(cm[0,0].sum()/cm[0].sum(),3))
+    Precision=cm[0:2,0:2].sum()/cm[0:2,].sum()
+    print("PPV (aka Precision): ",round(Precision,3))
     print("NPV: ",round(cm[2,2]/cm[2].sum(),3))
+    print("F1 Score: ",round(2*Precision*Recall/(Precision+Recall),3))
+
     return 
 
